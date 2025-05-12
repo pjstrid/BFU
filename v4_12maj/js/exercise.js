962f1b656e937cf3a466b60a32c1d1ef29918f40
jonathan strid <pjstrid@outlook.com>
2025-05-12 18:30:27 +0200

    completed exercises JS

---
+++ v4_12maj/js/exercise.js
@@ -0,0 +1,78 @@
+// let a = 10;
+// let b = 20;
+
+// if (a > b) {
+//     console.log(a, "är större än", b, true);
+// } else {
+//     console.log(a, "är mindre än", b, true);
+// }
+
+// let fruitA = "apple";
+// let fruitB = "pear";
+
+// if(a==b) {
+//     console.log("äpple är samma som päron" + true);
+// }
+// if (a!=b) {
+//     console.log("äpple är inte samma som päron: " + true);
+// }
+
+// const number = prompt("Skriv in din ålder i siffror");
+// console.log("Du är", number, "år");
+
+// const numberA = prompt("Skriv ett valfritt tal");
+// const numberB = prompt("Skriv ett till valfritt tal");
+
+// if (numberA > numberB) {
+//     console.log(numberA + " är störst");
+// } else {
+//     console.log(numberB + " är störst");
+// }
+
+// const numberOddOrEven = prompt("Skriv ännu ett valfritt tal");
+
+// if ( numberOddOrEven % 2 == 0) {
+//     console.log(numberOddOrEven, "är ett jämnt tal");
+// } else {
+//     console.log(numberOddOrEven, "är ett udda tal");
+// }
+
+// const lenght = prompt("Skriv din längd");
+// if (lenght >= 150) {
+//     console.log("Får jag åka Helix", true);
+// } else {
+//     console.log("Får jag åka Helix", false);
+// }
+
+
+
+ let small = "small";
+ let medium = "medium";
+ let large = "large";
+ let platinum = "platinum";
+
+
+ const ticket = prompt("Välkommen till Balder! Vilket åkband har du? small/medium/large/platinum");
+
+ if (ticket == large || ticket == platinum) {
+     console.log("Får du åka balder?:", true);
+ } else {
+     console.log("Får du åka Balder?:",false, "Köp ett nytt åkband");
+ }
+
+// const weight = prompt("Skriv din vikt i kg");
+// const lenght = prompt("Skriv din längd i cm");
+
+// console.log("Du väger:", weight, "kg");
+// console.log("Din längd:", lenght, "cm");
+// let sum = (weight / (lenght/100*lenght/100));
+
+// if (sum < 18.5) {
+//     console.log(sum, "Det är underviktig enligt BMI");
+// } else if (sum >18.4 && sum <25.1) {
+//     console.log(sum, "Det har normalvikt enligt BMI");
+// } else if (sum >25 && sum <30.1) {
+//     console.log(sum, "Det är överviktig enligt BMI");
+// } else if (sum >30) {
+//     console.log(sum, "Det är fetma enligt BMI");
+// }
---
+++ v4_12maj/js/function.js
@@ -0,0 +1,34 @@
+//Funktioner
+function add(numberOne, numberTwo) {
+    console.log("Add")
+    console.log("NumberOne är:", numberOne);
+    console.log("NumberTwo är:", numberTwo);
+    let sum = Number(numberOne) + Number(numberTwo);
+    
+    console.log("Summan blir:", sum);
+}
+
+function subtract(numberOne, numberTwo) {
+    console.log("Subtract");
+    let sum = numberOne - numberTwo;
+
+    console.log("Summan blir:", sum)
+}
+
+const inputNumberOne = prompt("Välj första nummer")
+console.log("Första nummer:", inputNumberOne)
+
+const inputOperator = prompt("Välj addering (+) eller subtrahering (-)")
+console.log("Operator:", inputOperator)
+
+const inputNumberTwo = prompt("Välj andra nummer")
+console.log("Andra nummer:", inputNumberTwo)
+
+if(inputOperator== "+") {
+    add(inputNumberOne, inputNumberTwo)
+} else if ( inputOperator== "-") {
+    subtract(inputNumberOne, inputNumberTwo)
+}
+
+//add(5, 10);
+//add(25, 55);
