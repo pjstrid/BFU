0cbb81dbedb53671a1c7ecde5690fb061c6060ae
jonathan strid <pjstrid@outlook.com>
2025-05-21 23:07:01 +0100

    created and completed arrays boot camp

--- v4_12maj/js/function.js
+++ v4_12maj/js/function.js
@@ -1,91 +1,112 @@
-const bankAccount = {
-    accountHolder: "Philip",
-    balance: 0,
-    deposit: function(amount) {
-        this.balance += amount;
-        console.log(amount + " deposited");
-        return this.balance;
-    },
-    withdraw: function(amount) {
-        this.balance -= amount;
-        console.log(amount + " withdrawn")
-        return this.balance;
 
-    },
-    showBalance() {
-        console.log("Your balance is: " + this.balance)
-    }
+
+
+
+
+
+
+
+
+
+
+/* const numberOne = prompt("Välj ett tal");
+const numberTwo = prompt("Välj ett till tal");
+const operator = prompt("Välj en operator: '+', '-', '/' eller '*'?");
+
+let numOne = Number(numberOne);
+let numTwo = Number(numberTwo);
+
+function math () {
+    if (operator == "+") {
+       return numOne + numTwo;
+    } else if (operator == "-") {
+       return  numOne - numTwo;
+    } else if (operator == "/") {
+       return numOne / numTwo;
+    } else if (operator == "*") {
+       return numOne * numTwo;
 }
-bankAccount.deposit(5000)
-console.log(bankAccount.balance);
-bankAccount.withdraw(1000)
-console.log(bankAccount.balance);
+}
+
+console.log (numOne + operator + numTwo + "=" + math());
+ */
+
+// const date = prompt("Skriv ett valfritt datum med följande design'YYYY-MM-DD'")
+
+// let year = date.substring(0, 4)
+
+// console.log( "Året var: " + (year));
+
+
+// function getStringLength() {
+//     return sentence.length
+// }
+
+//     const sentence = prompt("Skriv en valfri mening, så räknar jag ut längden")
+
+
+
+// console.log("Längden på denna sträng: " + "'"+ sentence +"'" + " är: " + getStringLength())
 
 
 
 
 
+// const bankAccount = {
+//     accountHolder: "Philip",
+//     balance: 0,
+//     deposit: function(amount) {
+//         this.balance += amount;
+//         console.log(amount + " deposited");
+//         return this.balance;
+//     },
+//     withdraw: function(amount) {
+//         this.balance -= amount;
+//         console.log(amount + " withdrawn")
+//         return this.balance;
+
+//     },
+//     showBalance() {
+//         console.log("Your balance is: " + this.balance)
+//     }
+// }
+// bankAccount.deposit(5000)
+// console.log(bankAccount.balance);
+// bankAccount.withdraw(1000)
+// console.log(bankAccount.balance);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Funktioner (från förläsningen med Christoffer)
-// function add(numberOne, numberTwo) {
-//     console.log("Add")
-//     console.log("NumberOne är:", numberOne);
-//     console.log("NumberTwo är:", numberTwo);
-//     let sum = Number(numberOne) + Number(numberTwo);
+// //Funktioner (från förläsningen med Christoffer)
+// // function add(numberOne, numberTwo) {
+// //     console.log("Add")
+// //     console.log("NumberOne är:", numberOne);
+// //     console.log("NumberTwo är:", numberTwo);
+// //     let sum = Number(numberOne) + Number(numberTwo);
     
-//     console.log("Summan blir:", sum);
-// }
+// //     console.log("Summan blir:", sum);
+// // }
 
-// function subtract(numberOne, numberTwo) {
-//     console.log("Subtract");
-//     let sum = numberOne - numberTwo;
+// // function subtract(numberOne, numberTwo) {
+// //     console.log("Subtract");
+// //     let sum = numberOne - numberTwo;
 
-//     console.log("Summan blir:", sum)
-// }
+// //     console.log("Summan blir:", sum)
+// // }
 
-// const inputNumberOne = prompt("Välj första nummer")
-// console.log("Första nummer:", inputNumberOne)
+// // const inputNumberOne = prompt("Välj första nummer")
+// // console.log("Första nummer:", inputNumberOne)
 
-// const inputOperator = prompt("Välj addering (+) eller subtrahering (-)")
-// console.log("Operator:", inputOperator)
+// // const inputOperator = prompt("Välj addering (+) eller subtrahering (-)")
+// // console.log("Operator:", inputOperator)
 
-// const inputNumberTwo = prompt("Välj andra nummer")
-// console.log("Andra nummer:", inputNumberTwo)
+// // const inputNumberTwo = prompt("Välj andra nummer")
+// // console.log("Andra nummer:", inputNumberTwo)
 
-// if(inputOperator== "+") {
-//     add(inputNumberOne, inputNumberTwo)
-// } else if ( inputOperator== "-") {
-//     subtract(inputNumberOne, inputNumberTwo)
-// }
+// // if(inputOperator== "+") {
+// //     add(inputNumberOne, inputNumberTwo)
+// // } else if ( inputOperator== "-") {
+// //     subtract(inputNumberOne, inputNumberTwo)
+// // }
 
-//add(5, 10);
-//add(25, 55);
+// //add(5, 10);
+// //add(25, 55);
---
+++ v5_19maj/js/arrays.js
@@ -0,0 +1,63 @@
+let str = "Supercalifragilisticexpialidocious";
+console.log(str);
+console.log("Finns det ett 'n'? = " + str.includes("n")); // includes ger true/false i detta fallet false
+console.log("Finns det ett 'x'? = " + str.includes("x")); // includes ger true/false i detta fallet true
+console.log("På vilken plats ligger första 'p:et'? = " + str.indexOf("p")); // indexOf ger siffra på platseen i strängen
+let fiveFirst = str.slice(0,5); // plockar ut från plats 0 till 5, alltså 0-4. inte till och med 5
+console.log(fiveFirst);
+let sevenLast = str.slice(str.length -7); // plockar ut de sju sista. alltså oavsett längd på sträng och minus 7 
+console.log(sevenLast);
+
+
+/* let names = ["Pelle","Olle","Kalle","Bertil"];
+console.log(names);
+names.splice (1, 2); // tar bort från plats 1, 2 stycken
+console.log(names);
+
+let nums = [1,2,3,4,5,6,7,8,9];
+console.log(nums);
+nums.sort();
+nums.reverse();
+console.log(nums); */
+
+
+/* let fruits = ["kiwi", "apple", "pear"];
+console.log(fruits);
+fruits.push("banana"); // lägger till en längst bak
+console.log(fruits);
+fruits.unshift("plum"); // lägger till en längst fram
+console.log(fruits);
+fruits.pop(); // tar bort en längst bak
+console.log(fruits);
+fruits.shift(); // tar bort en längst fram
+console.log(fruits);
+fruits.splice(1, 0, "melon"); // lägger till en på vald plats (1) och tar bort valt antal(0)
+console.log(fruits);
+fruits.splice(2, 2, "lime", "lemon", "orange"); // samma som ovan men tar också bort ett valt antal från den platsen och bakåt
+console.log(fruits) */
+
+/* let arr = ["a", "b", "c"];
+console.log(arr)
+let all = arr.slice(); // klonat samma array till en ny variabel
+console.log(all); */
+
+/* let a = [1,2,3];
+let b = [4,5,6];
+console.log(a);
+console.log(b);
+
+let c = a.concat(b); // lägger ihop arrayerna
+console.log(c)
+ */
+
+
+/* let animals = ["cat", "hamster", "parrot", "funky dog"];
+console.log(animals);
+console.log("Antal djur: " + animals.length);
+
+console.log("Första djuret är: " + animals[0]);
+
+console.log("Sista djuret är: " + animals[3]);
+
+animals[1] = "tiger";
+console.log("Andra djuret är utbytt från 'hamster' till: " + animals[1]); */
