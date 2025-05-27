4aa0c6aaac8abbdcab152d1693a22122e34fe97e
jonathan strid <pjstrid@outlook.com>
2025-05-27 11:58:34 +0200

    Update dom.js

--- v6_26maj/js/dom.js
+++ v6_26maj/js/dom.js
@@ -1,11 +1,16 @@
 /* 
 let elem = document.querySelector(cssSelector);
+  Tar in en cssSelector (ID, class, tag) flexibel
+    Hämtar det första elementet den träffar, ex knappar med samma klass
 
 let elem = document.querySelectorAll(cssSelector);
-    Returnerar en lista med alla element den hittar med den selektorn
+  Returnerar en lista med alla element den hittar med den selektorn
 
 let elem = document.getElementById("myId");  
+  Hämtar ett element enbart baserat på ID som finns i html
 
+let elem = document.getELementsByClass("myClass")
+  Hämtar en array(en lista) med alla element i den klassen, inte endast ett element  
 */
 
 const todoListElem = document.querySelector("#todo-list");
